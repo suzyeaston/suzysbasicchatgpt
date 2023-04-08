ca38bf4d867215977f0095e2da9abc3fb52a021c
Suzy Easton <suzyeaston@icloud.com>
2023-04-08 14:24:37 -0700

    sometimes i'm silly

---
+++ chatgpt_api_example.py
@@ -0,0 +1,19 @@
+import openai
+import os
+
+openai.api_key = "yourtoken"
+
+def generate_response(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    return response.choices[0].message['content'].strip()
